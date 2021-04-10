d23f9f62f148f21461b0215a01d0f6a460ece8a2
Дмитрий Игошев <digoschev@yandex.ru>
2021-04-10 09:09:16 +0300

    add docs

--- homework.py
+++ homework.py
@@ -1,8 +1,16 @@
+"""
+Данный модуль содержит классы Record, Calculator, CaloriesCalculator,
+CashCalculator. Данные классы нужны для подсчета расходов и доходов.
+"""
 import datetime as dt
 from typing import Optional
 
 
 class Record:
+    """
+    Класс Record создаёт объект со свойствами amount, comment, date.
+    """
+
     def __init__(self, amount: int, comment: str, date: Optional[str] = None):
         self.amount = amount
         self.comment = comment
@@ -13,25 +21,51 @@ class Record:
 
 
 class Calculator:
+    """
+    Класс Calculator содержит методы add_record, get_today_stats,
+    get_week_stats. Содержит свойства limit и список объектов-записей records
+    """
+
     def __init__(self, limit: int):
         self.limit = limit
         self.records: list = []
 
     def add_record(self, record: object) -> None:
+        """
+        Метод add_record добавляет объекты к списку records
+        """
         self.records.append(record)
 
     def get_today_stats(self) -> int:
+        """
+        Метод get_today_stats возвращает сумму набранных калорий
+        или потраченных денег за сегодняшний день.
+        """
         return sum(rec.amount for rec in self.records
                    if rec.date == dt.date.today())
 
     def get_week_stats(self) -> int:
+        """
+        Метод get_week_stats возвращает сумму набранных калорий
+        или потраченных денег за прошедшую неделю.
+        """
         week_ago: dt.date = dt.date.today() - dt.timedelta(days=7)
         return sum(rec.amount for rec in self.records
                    if dt.date.today() >= rec.date >= week_ago)
 
 
 class CaloriesCalculator(Calculator):
+    """
+    Класс CaloriesCalculator наследует функционал родительского класса
+    Calculator. Содержит метод get_calories_remained. Принимает как аргумент
+    лимит калорий на сегодня.
+    """
+
     def get_calories_remained(self) -> str:
+        """
+        Метод get_calories_remained возвращает строку-рекомендацию о
+        потреблении калорий.
+        """
         calories: int = self.get_today_stats()
         if calories < self.limit:
             return ('Сегодня можно съесть что-нибудь ещё, но с общей '
@@ -42,11 +76,21 @@ class CaloriesCalculator(Calculator):
 
 
 class CashCalculator(Calculator):
+    """
+    Класс CashCalculator наследует функционал родительского класса Calculator.
+    Содержит метод get_today_cash_remained. Принимает как аргумент лимит денег
+    на сегодня. Также содержит курсы валют.
+    """
+
     USD_RATE = 78.0
     EURO_RATE = 92.0
     RUB_RATE = 1.0
 
     def get_today_cash_remained(self, currency: str) -> str:
+        """
+        Метод get_calories_remained возвращает строку-рекомендацию о расходе
+        денег. Принимает как аргумент код валюты в виде строки.
+        """
         money: int = self.limit - self.get_today_stats()
 
         if money == 0:
